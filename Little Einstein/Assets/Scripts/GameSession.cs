b67fa74bb8fef9afc0037c146411e19501a62e91
Mads Thomsen <Kjaersgaard_fkt@hotmail.com>
2018-10-24 12:47:21 +0200

    Bugs and fixes

---
+++ Little Einstein/Assets/Scripts/GameSession.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameSession : MonoBehaviour {
+
+    [SerializeField]    int playerLives = 3;
+    [SerializeField]    int score = 0;
+
+    [SerializeField]    Text liveText;
+    [SerializeField]    Text scoreText;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+		
+	}
+    void Start()
+    {
+        liveText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if (playerLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    private void TakeLife()
+    {
+        playerLives--;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        liveText.text = playerLives.ToString();
+
+    }
+
+    private void ResetGameSession()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
+
+    
